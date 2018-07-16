597eba93ab8ae78b4ee7afc4c23d745d1614250e
Valeran86 <uq8cysjd>
2018-07-17 00:59:40 +0500

    initial commit

---
+++ src/main/java/ru/sbt/serialization/SomePoint.java
@@ -0,0 +1,30 @@
+package ru.sbt.serialization;
+
+/**
+ * Created by Home on 17.07.2018.
+ */
+public class SomePoint {
+    private String name;
+
+    private double x;
+
+    private double y;
+
+    protected SomePoint( double x, double y, String name ) {
+        this.x = x;
+        this.y = y;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+}
